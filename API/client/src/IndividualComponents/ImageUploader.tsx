0380657a6d9d2372940fe15ed33e8010d90a6813
bbastanza <b.bastanza@gmail.com>
2021-03-06 11:11:54 -0800

    commit remove react-aws-s3 => moving to amplify

--- API/client/src/IndividualComponents/ImageUploader.tsx
+++ API/client/src/IndividualComponents/ImageUploader.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import S3 from "react-aws-s3";
 
 interface IAwsConfig {
     bucketName: string;
@@ -9,14 +8,6 @@ interface IAwsConfig {
     secretAccessKey: string;
 }
 
-const config: IAwsConfig = {
-    bucketName: process.env.REACT_APP_AWS_BUCKET,
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
-    dirName: "Images/",
-    region: "us-east-1",
-};
-
 interface ImageUploaderProps {
     returnImage: Function;
 }
@@ -24,16 +15,12 @@ interface ImageUploaderProps {
 const ImageUploader: React.FC<ImageUploaderProps> = ({ returnImage }) => {
     const [event, setEvent] = useState<any>(null);
 
-    console.log(config);
     const uploadImage = async () => {
         const file = event.target.files[0];
         if (file.size > 2000000) {
             console.log("too big");
             return "nope";
         }
-        const awsClient = new S3(config);
-        const response = await awsClient.uploadFile(file);
-        console.log(response);
         returnImage(file);
         return "hi";
     };
